883af2ea3d7f9256802599ed50eff9c6eb847b88
Aravindalochanan1996 <lochanan1996@gmail.com>
2026-04-30 10:38:02 +0530

    Config Changes

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,4 +1,4 @@
-INPUT_DIR = "C:/Users/Abuthahir Abdul/OneDrive - D4 Technologies Inc.,/Timesheet"
+INPUT_DIR = "C:/Users/aravinalochanan/OneDrive - D4 Technologies Inc.,/Timesheet"
 PROCESSED_DIR = "app/data/processed"
 OUTPUT_FILE = "app/data/output/timesheet_report.xlsx"
 LOG_FILE = "app/logs/app.log"   
